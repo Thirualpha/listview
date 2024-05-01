b70dfeabba16f0af9fc76ee6d6acc5aa1c6ab4ff
Thirualpha <113031702+Thirualpha@users.noreply.github.com>
2024-05-01 10:04:21 +0530

    Update CustomAdapter.java

--- app/src/main/java/com/example/listview/CustomAdapter.java
+++ app/src/main/java/com/example/listview/CustomAdapter.java
@@ -1,1 +1,50 @@
+package com.example.imagelist;
 
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+public class CustomAdapter extends BaseAdapter {
+    Context context;
+    String countryList[];
+    int flags[];
+    LayoutInflater inflter;
+
+    public CustomAdapter(Context applicationContext, String[] countryList, int[] flags) {
+        this.context = context;
+        this.countryList = countryList;
+        this.flags = flags;
+        inflter = (LayoutInflater.from(applicationContext));
+    }
+
+    @Override
+    public int getCount() {
+        return countryList.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        view = inflter.inflate(R.layout.activity_listview, null);
+        TextView country = (TextView) view.findViewById(R.id.textView3);
+        ImageView icon = (ImageView) view.findViewById(R.id.imageView10);
+        country.setText(countryList[i]);
+        icon.setImageResource(flags[i]);
+        return view;
+    }
+}
